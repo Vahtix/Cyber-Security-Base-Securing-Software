ad806743e8aa8d8233ce48bcb34cf037a9978da1
Vahtix <vahtis91@gmail.com>
2017-07-13 20:14:34 +0300

    Using lists in Spring Boot

---
+++ Part1/05.HelloList/src/main/java/sec/hellolist/HelloListController.java
@@ -0,0 +1,33 @@
+package sec.hellolist;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloListController {
+
+    private List<String> list;
+
+    public HelloListController() {
+        this.list = new ArrayList<>();
+        this.list.add("Hello world!");
+        this.list.add("eka");
+        this.list.add("toka");
+        this.list.add("kolmas");
+    }
+
+    @RequestMapping("/")
+    public String home(Model model, @RequestParam(required = false) String content) {
+        if (content != null && !content.trim().isEmpty()) {
+            this.list.add(content.trim());
+        }
+
+        model.addAttribute("list", list);
+        return "index";
+    }
+
+}
