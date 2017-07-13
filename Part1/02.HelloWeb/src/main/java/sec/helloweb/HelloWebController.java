7ddee2d13d899a485af499d22d863a4f30376f53
Vahtix <vahtis91@gmail.com>
2017-07-13 19:45:58 +0300

    Hello world with Springboot

---
+++ Part1/02.HelloWeb/src/main/java/sec/helloweb/HelloWebController.java
@@ -0,0 +1,15 @@
+package sec.helloweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWebController {
+
+    @RequestMapping("*")
+    @ResponseBody
+    public String home() {
+        return "Hello Web!";
+    }
+}
